75f2dcde2dda8876a0de0e8d8dcb563621dfb5fd
k-main <kennkmaina@mgail.com>
2023-07-19 19:37:13 -0700

    Attempted to make main script more readable

--- run.sh
+++ run.sh
@@ -1,4 +1,113 @@
 #!/bin/bash
+
+echo "==============Ebay Webscraper=============="
+echo Prerequisites: python3-venv, BeautifulSoup4, lxml
+source env/bin/activate
+
+function Execute(){
+    echo Run:
+    echo "1) Macbooks Pros"
+    echo "2) Macbooks & Macbook Pros"
+    read entry
+    case $entry in
+        1)
+            echo Selected: Macbook Pros
+            pageNum=1
+        ;;
+        2)
+            echo "Selected: Macbooks & Macbook Pros"
+            pageNum=5
+        ;;
+    esac
+    echo Retrieving bulk data via wget...
+    echo Clearing FilteredOutput.txt...
+    echo Clearing bin/save_loc.txt...
+    > FilteredOutput.txt
+    > bin/save_loc.txt
+    echo '0' > bin/save_loc.txt
+    pageLim=$(( $pageNum + 3 ))
+    fileNum=1
+    while [ $pageNum -le $pageLim ];
+        do 
+            clear
+            echo Retrieving bulk data via wget...
+            lineNum=$( head -n $pageNum wget-links.txt | tail -1 )
+            echo -n "Retrieving $fileNum/4: $lineNum"
+            wget -q -O index$fileNum.html $lineNum
+            pageNum=$(( $pageNum + 1 ))
+            fileNum=$(( $fileNum + 1 ))
+        done
+        echo "Pushing output to bin... Done"
+        mv index* bin/
+        echo "Filtering... Done"
+        python3 stringProcessor.py
+        echo "Outputting links to FilteredOutput.txt... Done"
+        echo
+}
+
+function Clear(){
+    echo -n Clearing bin... 
+    rm bin/index* 2>> error_log.txt
+    if [ $? == 0 ]
+        then
+            echo OK.
+
+        else
+            echo Failed: Nothing to clear.
+    fi
+    #echo done.
+    echo
+}
+
+function Regenerate(){
+    if [ -f bin/index1.html ];
+        then
+        > FilteredOutput.txt
+        > bin/save_loc.txt
+        echo '0' > bin/save_loc.txt
+        python3 stringProcessor.py
+        echo
+        else
+        echo No bulk data to regenerate from.
+    fi
+}
+
+function readOutput(){
+    linkTotal=$( wc -l FilteredOutput.txt | cut -d " " -f 1 )
+    linkTotal=$(( $linkTotal / 2 ))
+    savePosition=$( cat bin/save_loc.txt )
+    iteration=1
+    linkNum=1
+    if [ $savePosition != '0' ]
+        then
+        echo "Continue from previous save point, $savePosition/$linkTotal? (Y/n)"
+        echo "'x' to return"
+        read val
+        if [ $val != 'n' ]
+            then
+            linkNum=$savePosition
+        fi
+    fi
+
+    option='n'
+    while [ $option != 'x' ]
+        do
+        clear
+        echo "Enter 'x' to exit"
+        echo "Enter any other key to continue"
+        echo "Opening link $iteration of $linkTotal"
+        linkName=$(( $linkNum + 1 ))
+        head -n $linkName FilteredOutput.txt | tail -1
+        hyperLink=$( head -n $linkNum FilteredOutput.txt | tail -1 )
+        firefox $hyperLink
+        echo $linkNum > bin/save_loc.txt
+        #editing
+        linkNum=$(( $linkNum + 2 ))
+        iteration=$(( $iteration + 1 ))
+        read option
+        done
+}
+
 function Menu()
 {
 echo "==============Ebay Webscraper=============="
@@ -13,10 +122,6 @@ echo -n "Option: "
 read input
 }
 
-
-echo "==============Ebay Webscraper=============="
-echo Prerequisites: python3-venv, BeautifulSoup4, lxml
-source env/bin/activate
 sleep 1
 clear
 Menu
@@ -24,99 +129,16 @@ while [ $input ]
     do
     case $input in
         1)
-            echo Run:
-            echo "1) Macbooks Pros"
-            echo "2) Macbooks & Macbook Pros"
-            read entry
-            case $entry in
-                1)
-                    echo Selected: Macbook Pros
-                    pageNum=1
-                ;;
-                2)
-                    echo "Selected: Macbooks & Macbook Pros"
-                    pageNum=5
-                ;;
-            esac
-            echo Retrieving bulk data via wget...
-            echo Clearing FilteredOutput.txt...
-            echo Clearing bin/save_loc.txt...
-            > FilteredOutput.txt
-            > bin/save_loc.txt
-            echo '0' > bin/save_loc.txt
-            pageLim=$(( $pageNum + 3 ))
-            fileNum=1
-            while [ $pageNum -le $pageLim ];
-                do 
-                    clear
-                    echo Retrieving bulk data via wget...
-                    lineNum=$( head -n $pageNum wget-links.txt | tail -1 )
-                    echo -n "Retrieving $fileNum/4: $lineNum"
-                    wget -q -O index$fileNum.html $lineNum
-                    pageNum=$(( $pageNum + 1 ))
-                    fileNum=$(( $fileNum + 1 ))
-                done
-                echo "Pushing output to bin... Done"
-                mv index* bin/
-                echo "Filtering... Done"
-                python3 stringProcessor.py
-                echo "Outputting links to FilteredOutput.txt... Done"
-                echo
+            Execute
         ;;
         2)
-            echo -n Clearing bin... 
-            rm bin/index* 2>> error_log.txt
-            if [ $? == 0 ]
-                then
-                    echo OK.
-
-                else
-                    echo Failed: Nothing to clear.
-            fi
-            #echo done.
-            echo
+            Clear
         ;;
         3)
-            > FilteredOutput.txt
-            > bin/save_loc.txt
-            echo '0' > bin/save_loc.txt
-            python3 stringProcessor.py
-            echo
+            Regenerate
         ;;
         4)
-            linkTotal=$( wc -l FilteredOutput.txt | cut -d " " -f 1 )
-            linkTotal=$(( $linkTotal / 2 ))
-            savePosition=$( cat bin/save_loc.txt )
-            iteration=1
-            linkNum=1
-            if [ $savePosition != '0' ]
-                then
-                echo "Continue from previous save point, $savePosition/$linkTotal? (Y/n)"
-                echo "'x' to return"
-                read val
-                if [ $val != 'n' ]
-                    then
-                    linkNum=$savePosition
-                fi
-            fi
-
-            option='n'
-            while [ $option != 'x' ]
-                do
-                clear
-                echo "Enter 'x' to exit"
-                echo "Enter any other key to continue"
-                echo "Opening link $iteration of $linkTotal"
-                linkName=$(( $linkNum + 1 ))
-                head -n $linkName FilteredOutput.txt | tail -1
-                hyperLink=$( head -n $linkNum FilteredOutput.txt | tail -1 )
-                firefox $hyperLink
-                echo $linkNum > bin/save_loc.txt
-                #editing
-                linkNum=$(( $linkNum + 2 ))
-                iteration=$(( $iteration + 1 ))
-                read option
-                done
+            readOutput
         ;;
         5)
             echo Exiting...
