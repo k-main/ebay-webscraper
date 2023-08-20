548157da6210b999dad84afdef0c6aaa29537ffc
k-main <kennkmaina@mgail.com>
2023-08-20 04:00:03 -0700

    just threw everything into a big list

--- src/cost_analysis.py
+++ src/cost_analysis.py
@@ -22,33 +22,30 @@ def det_nonestr(detail):
     return detail
 
 def find_genericboards():
+    search_return = []
     cursor.execute('SELECT categorization, link FROM boards')
     items = cursor.fetchall()
     for item in items:
         none_arr = [' ', 'None', 'None']
         item_desc = str(item)[2:-3].split(" ")
         item_desc[2] = item_desc[2][:-2]
-        #print(item_desc[:-1])
-
         
         if (item_desc[1:-1] == none_arr[1:]):
             continue
-
-        
+  
         sqlcomm_str = 'SELECT * FROM generic_boards WHERE type LIKE ? AND model_num LIKE ? AND year LIKE ?'
         cursor.execute(sqlcomm_str, ('%' + det_nonestr(item_desc[0]) + '%','%' + det_nonestr(item_desc[1]) + '%','%' + det_nonestr(item_desc[2]) + '%'))
         results = cursor.fetchall()
         
-        print("{} ->".format([item_desc[:-1], item_desc[3][:10]]))
+        #print("{} ->".format([item_desc[:-1], item_desc[3][:10]]))
         if(len(results) > 5):
-            print("{} Results".format(len(results)))
+            continue
         else:
-            [print(result) for result in results]
-        print()
-        
-    return
+            search_return.append([item_desc,  results])
 
-find_genericboards()
-#read_all("boards")
+    return search_return
+
+
+generic_boards = find_genericboards()
 
 conn.close()
